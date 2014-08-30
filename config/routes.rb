687327fe40d897a51a0bcb1310edb9f963c78ded
perplexicon <alexgarkavenko@gmail.com>
2014-08-30 14:43:25 +1200

    Redirect to dashboard upon sign in
    * Add monban constraints to routes file
    * Set root route as dashboards#show for signed in users
    * Create dashboards controller with show action
    * Create dashboards#show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+require "monban/constraints/signed_in"
+
 Rails.application.routes.draw do
+  constraints Monban::Constraints::SignedIn.new do
+    root "dashboards#show", as: :dashboard
+  end
+  
   root "homes#show"
 
   resource :session, only: [:new, :create, :destroy]
